0583a30539de003bee6f9c6c61b9ef3afec03a4b
Dawe Xu <isxu@outlook.com>
2021-09-29 22:51:46 +0800

    scalac + filename

--- hello.scala
+++ hello.scala
@@ -1,1 +1,10 @@
-println("hello-world-")  // output "hello-world-"
+object hello_world_ {
+    /* this is first scala program
+     *
+     */
+     def main (args:Array[String]) {
+         println("hello-world-")  // output "hello-world-"
+     } 
+}
+;
+
